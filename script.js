871e6fd0a565df44cb7e327101f68b718358cd62
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2020-05-04 11:45:28 +0100

    added search bar

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('nothing to see here')
+window.onload = (event) => {
+    let search = document.getElementById('search')
+    search.focus()
+};
+
+search.addEventListener('keydown', event => {
+    if (event.keyCode === 13) {
+        console.log('going')
+        window.location.href = `https://www.google.com/search?q=${search.value}`
+    }
+})
